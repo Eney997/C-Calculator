04ff5ebbbe38d5f53c426486d1a0023b388ce1a8
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2024-03-22 14:21:15 +0400

    changedPosition

--- main.c
+++ main.c
@@ -3,29 +3,28 @@
 
 int main()
 {
-    double num1;
-    double num2;
+    double num1,num2;
     char op;
 
-    printf("Enter a number : ");
+    printf("Enter your first number: ");
     scanf("%lf",&num1);
 
-    printf("Enter operator: ");
-    scanf(" %c", &op);
-
-    printf("Enter a number : ");
+    printf("Enter your second number: ");
     scanf("%lf",&num2);
 
-    if(op == '+'){
-      printf("%f", num1 + num2);
-    }else if(op == '-'){
-      printf("%f", num1 - num2);
-    }else if(op == '/'){
-      printf("%f", num1 / num2);
-    }else if(op == '*'){
-      printf("%f", num1 * num2);
-    }else {
-     printf("Invalid Operator");
+    printf("Enter your operator: ");
+    scanf(" %c",&op);
+
+    if(op=='+'){
+        printf("%f",num1+num2);
+    }else if(op=='-'){
+        printf("%f",num1-num2);
+    }else if(op=='*'){
+        printf("%f",num1*num2);
+    }else if(op=='/'){
+        printf("%f",num1/num2);
+    }else{
+        printf("Invalid number");
     }
 
     return 0;
